78d4763ac7964c8867d55ae8cca8d50537fe54d8
Tozhybayev Aibat <a.tozhybayev@alantech.kz>
2024-05-20 20:51:35 +0500

    login logout and signup functions added

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -15,12 +18,33 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredEmail = '';
   var _enteredPassword = '';
 
-  void _submit() {
+  void _submit() async {
     final isValid = _formKey.currentState!.validate();
-    if(isValid) {
-      _formKey.currentState!.save();
-      print(_enteredPassword);
-      print(_enteredEmail);
+
+    if (!isValid) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+    try {
+      if (_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+      } else {
+        final userCredentials = await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+      }
+    } on FirebaseAuthException catch (error) {
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.message ?? 'Authentication failed'),
+        ),
+      );
     }
   }
 
@@ -60,7 +84,9 @@ class _AuthScreenState extends State<AuthScreen> {
                             autocorrect: false,
                             textCapitalization: TextCapitalization.none,
                             validator: (value) {
-                              if(value == null || value.trim().isEmpty || !value.contains('@')) {
+                              if (value == null ||
+                                  value.trim().isEmpty ||
+                                  !value.contains('@')) {
                                 return 'Please enter a valid email address';
                               }
                               return null;
@@ -74,7 +100,7 @@ class _AuthScreenState extends State<AuthScreen> {
                                 const InputDecoration(label: Text('Password')),
                             obscureText: true,
                             validator: (value) {
-                              if(value == null || value.trim().length < 6) {
+                              if (value == null || value.trim().length < 6) {
                                 return 'Password mus be at least 6 character long';
                               }
                               return null;
